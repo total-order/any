7956e9242e73f2d3c922c77b20fd60909709cc6d
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-09-01 23:41:41 +0200

    :sparkles: feat: First draft.

---
+++ src/index.js
@@ -0,0 +1,158 @@
+import assert from 'assert';
+import {
+	increasing as compare,
+	quasilexicographical,
+	attr,
+} from '@aureooms/js-compare';
+// Import {zip} from '@aureooms/js-itertools';
+
+// Const compareIterables = (a, b) => {
+// for (const [left, right] of zip(a, b)) {
+// const delta = compare(left, right);
+// if (delta !== 0) return delta;
+// }
+
+// return 0;
+// };
+
+const deepCompare = (a, b) => {
+	// TODO check for circular refs
+
+	if (a === b) return 0;
+
+	const left = [[a]];
+	const right = [[b]];
+	const pos = [0];
+
+	while (left.length !== 0) {
+		assert(left.length === right.length);
+		assert(left.length === pos.length);
+
+		const last = left.length - 1;
+		const array_a = left[last];
+		const array_b = right[last];
+		const i = pos[last];
+
+		assert(array_a.length === array_b.length);
+		assert(i >= 0 && i <= array_a.length);
+		if (i === array_a.length) {
+			left.pop();
+			right.pop();
+			pos.pop();
+			continue;
+		}
+
+		++pos[last];
+
+		const _a = array_a[i];
+		const _b = array_b[i];
+		const typeof_a = typeof _a;
+		const typeof_b = typeof _b;
+
+		const delta_type = compare(typeof_a, typeof_b);
+		if (delta_type !== 0) return delta_type;
+
+		if (typeof_a !== 'object') {
+			const delta_primitive = comparePrimitives(typeof_a, _a, _b);
+			if (delta_primitive !== 0) return delta_primitive;
+			continue;
+		}
+
+		assert(typeof_a === 'object');
+
+		const constructor_a = a.constructor;
+		const constructor_b = b.constructor;
+
+		if (constructor_a !== constructor_b)
+			return compare(constructor_a.name, constructor_b.name);
+
+		// TODO typed arrays
+		// TODO other built-ins, see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
+		switch (constructor_a) {
+			case RegExp:
+				const delta_regexp = compare(_a.toString(), _b.toString());
+				if (delta_regexp !== 0) return delta_regexp;
+				continue;
+			case Date:
+				const delta_date = compare(_a.getTime(), _b.getTime());
+				if (delta_date !== 0) return delta_date;
+				continue;
+			case Array:
+				if (_a.length !== _b.length) return _a.length - _b.length;
+				left.push(_a);
+				right.push(_b);
+				pos.push(0);
+				continue;
+			case Object:
+				const keys_a = Object.keys(_a).sort(compare);
+				const keys_b = Object.keys(_b).sort(compare);
+
+				const delta_keys = compareStringArrays(keys_a, keys_b);
+				if (delta_keys !== 0) return delta_keys;
+
+				const values_a = Object.entries(_a)
+					.sort(compareEntries)
+					.map((x) => x[1]);
+				const values_b = Object.entries(_b)
+					.sort(compareEntries)
+					.map((x) => x[1]);
+
+				left.push(values_a);
+				right.push(values_b);
+				pos.push(0);
+				continue;
+			default:
+				throw new Error(
+					`deepCompare: unhandled constructor '${constructor_a.name}'`,
+				);
+		}
+	}
+
+	return 0;
+};
+
+const comparePrimitives = (kind, _a, _b) => {
+	assert(kind !== 'object');
+	switch (kind) {
+		case 'number':
+			if (Number.isNaN(_a)) {
+				if (Number.isNaN(_b)) return 0;
+				return 1;
+			}
+
+			if (Number.isNaN(_b)) {
+				return -1;
+			}
+
+		case 'boolean':
+		case 'string':
+		case 'bigint':
+			return compare(_a, _b);
+		case 'function':
+			return compare(_a.toString(), _b.toString());
+		default:
+			throw new Error(`deepCompare: unhandled primitive type '${kind}'`);
+	}
+};
+
+const compareEntries = attr(compare, 0);
+// Const compareArrays = quasilexicographical(deepCompare);
+const compareStringArrays = quasilexicographical(compare);
+// Const compareObjects = (a, b) => {
+// const keys_a = Object.keys(a).sort(compare);
+// const keys_b = Object.keys(b).sort(compare);
+
+// const delta_keys = compareStringArrays(keys_a, keys_b);
+// if (delta_keys !== 0) return delta_keys;
+
+// const values_a = Object.entries(a)
+// .sort(compareEntries)
+// .map((x) => x[1]);
+// const values_b = Object.entries(b)
+// .sort(compareEntries)
+// .map((x) => x[1]);
+
+// return compareArrays(values_a, values_b);
+// };
+
+export default deepCompare;
---
+++ test/src/deepCompare.js
@@ -0,0 +1,117 @@
+import test from 'ava';
+
+import deepCompare from '../../src';
+
+const macro = (t, a, b, expected) => {
+	const result = deepCompare(a, b);
+	t.is(expected, result);
+	const reversedResult = deepCompare(b, a);
+	t.is(-expected | 0, reversedResult);
+};
+
+const repr = (x) => {
+	if (x instanceof RegExp) return x.toString();
+	if (x instanceof Date) return `Date(${x.getTime()})`;
+	if (x instanceof Function) return x.toString();
+	if (typeof x === 'bigint') return `${x.toString()}n`;
+	if (typeof x === 'number') return x.toString();
+	return JSON.stringify(x);
+};
+
+const expectedSymbol = (expected) => {
+	return expected < 0 ? '<' : expected > 0 ? '>' : '=';
+};
+
+macro.title = (title, a, b, expected) =>
+	title || `${repr(a)} ${expectedSymbol(expected)} ${repr(b)}`;
+
+test(macro, true, true, 0);
+test(macro, true, false, 1);
+test(macro, false, true, -1);
+test(macro, false, false, 0);
+test(macro, true, 1, -1);
+test(macro, true, 0, -1);
+test(macro, false, 1, -1);
+test(macro, false, 0, -1);
+test(macro, Number.NaN, Number.NaN, 0);
+test(macro, Number.NaN, 0, 1);
+test(macro, Number.NaN, 1, 1);
+test(macro, Number.NaN, -1, 1);
+test(macro, Number.NaN, Infinity, 1);
+test(macro, Number.NaN, -Infinity, 1);
+test(macro, Infinity, Infinity, 0);
+test(macro, -Infinity, -Infinity, 0);
+test(macro, -Infinity, Infinity, -1);
+test(macro, Infinity, -Infinity, 1);
+test(macro, 1, Infinity, -1);
+test(macro, -1, -Infinity, 1);
+test(macro, -1, Infinity, -1);
+test(macro, 1, -Infinity, 1);
+test(macro, 1, true, 1);
+test(macro, 1, false, 1);
+test(macro, 0, true, 1);
+test(macro, 0, false, 1);
+test(macro, 1, 1, 0);
+test(macro, 0, 1, -1);
+test(macro, 1, 0, 1);
+test(macro, 1, '', -1);
+test(macro, '', 1, 1);
+test(macro, '', '', 0);
+test(macro, '', 'a', -1);
+test(macro, 'a', '', 1);
+test(macro, 'a', 'a', 0);
+test(macro, 'a', undefined, -1);
+test(macro, undefined, 'a', 1);
+test(macro, undefined, undefined, 0);
+test(macro, null, null, 0);
+test(macro, null, undefined, -1);
+test(macro, undefined, null, 1);
+test(macro, undefined, {}, 1);
+test(macro, {}, undefined, -1);
+test(macro, {}, {}, 0);
+test(macro, {}, {a: 1}, -1);
+test(macro, {a: 1}, {}, 1);
+test(macro, {a: 1}, {a: 1}, 0);
+test(macro, {a: 1}, {b: 1}, -1);
+test(macro, {a: 1, b: 2}, {b: 2, a: 1}, 0);
+test(macro, {c: {a: 1, b: 2}}, {c: {b: 2, a: 1}}, 0);
+test(macro, /s/, /s/, 0);
+test(macro, /s/, /sa/, -1);
+test(macro, /sa/, /s/, 1);
+test(macro, new Date(0), new Date(0), 0);
+test(macro, new Date(0), new Date(1), -1);
+test(macro, new Date(1), new Date(0), 1);
+test(macro, new Date(1), /s/, -1);
+test(macro, /s/, new Date(1), 1);
+test(macro, [], [], 0);
+test(macro, [], [1], -1);
+test(macro, [], [-1], -1);
+test(macro, [1, 2, 3], [1, 2, 3], 0);
+test(macro, [3, 2, 1], [1, 2, 3], 1);
+test(macro, [1, 2, 3, 4], [1, 2, 3], 1);
+test(macro, [1, 2, 3, 4], [7, 8, 9], 1);
+test(
+	macro,
+	() => undefined,
+	() => undefined,
+	0,
+);
+test(
+	macro,
+	() => undefined,
+	() => 1,
+	1,
+);
+test(
+	macro,
+	() => 1,
+	() => undefined,
+	-1,
+);
+test(
+	macro,
+	() => 1,
+	() => 1,
+	0,
+);
+// Test(macro, function () { return 1 }, () => 1, ?); // babel duplicate
